9d3dc1a0bb178ec278ee611aa37bea9a53fb1bb8
zlatidancheva <85891027+zlatidancheva@users.noreply.github.com>
2021-06-21 17:04:42 +0300

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client
+{
+    [MessagePack.MessagePackObject]
+    public class Student
+    {
+
+        [MessagePack.Key(0)]
+        public int Id { get; set; }
+        [MessagePack.Key(1)]
+        public string FirstName { get; set; }
+        [MessagePack.Key(2)]
+        public string LastName { get; set; }
+        [MessagePack.Key(3)]
+        public int Hours { get; set; }
+
+    }
+}
